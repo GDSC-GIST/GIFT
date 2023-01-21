5e4d1167772cffdbb6d9ad5aaa9d1ed580c5dc57
leessunj <sj06152@gmail.com>
2023-01-22 02:06:16 +0900

    construct basic form of whatGift page

--- src/pages/whatgiftPage.jsx
+++ src/pages/whatgiftPage.jsx
@@ -1,55 +1,96 @@
-import React, { useState } from 'react';
-import BackButton from '../components/what-gift/BackButton';
-import NextButton from '../components/what-gift/NextButton';
+import React, { useState,useRef, useEffect } from 'react';
 import ProgressBar from '../components/what-gift/ProgressBar';
 import Question from '../components/what-gift/Question';
 import Answer from '../components/what-gift/Answer';
-import questions from '../assets/questions'
+import questions from '../assets/questions';
+import testImg from '../assets/result_img/test.png';
+import { useHistory } from 'react-router-dom';
 
 function WhatgiftPage() {
 
   questions=questions;
-  const [type,setType]=useState(0);
-  const [number,setNumber]=useState(1);
+  const [number,setNumber]=useState(1); //Q number
+  const type=useRef(0); //type of Q
+  const index=useRef(1); //Index of questions.json
+  const [answers,setAnswers]=useState([]);
   const [clicked,setClicked]=useState(0);
+  const ansClicked=useRef(false);
+  const qNum=useRef(12);
+  const history=useHistory();
+
   const onClick=(e)=>{
-    console.log(e.target.innerText);
-    setNumber((prevNum)=>prevNum+1);
+    ansClicked.current=true;
+    setAnswers([...answers,e.target.name]); //or e.target.innerText
   };
 
-  const makeQuestion=(opNum)=>{
-    if(opNum===2){
+  useEffect(()=>{ //answers의 값이 변할 때 실행
+    console.log(answers);
+
+    if(ansClicked.current){
+      index.current+=1;
+
+      if(number===qNum.current){
+        history.push('/result');
+      }
+      if(type.current===6 || number===6){
+        console.log("finished default Qs");
+        //*TODO: 서버에 answers 정보 보내기
+        const estimate=5;  //서버에서 정보(예측된 타입) 받아와서 넣을 예정 : 
+        //*TODO: 6번 타입일때 어떻게 처리할지 생각해봐야함
+        goSubType(estimate);
+      }
+      setNumber((prevNum)=>prevNum+1);
+    }
+    ansClicked.current=false;
+  },[answers]);
+
+  const goSubType=(estimateNum)=>{ //세부 타입 결정(다음 질문, 질문 개수 조정)
+    index.current=questions[0].infoIndex[estimateNum];
+    type.current=estimateNum;
+    
+    if(estimateNum===6) qNum.current+=1;
+    else qNum.current=qNum.current-6+questions[0].infoNum[estimateNum];
+  };
+
+  const makeQuestion=(q)=>{ //질문에 따라 나타나는 화면 결정
+    if(q.opNum===2){
       return (
         <>
-      <Answer onClick={onClick}>{questions[number].options[0]}</Answer>
-      <Answer onClick={onClick}>{questions[number].options[1]}</Answer>
-      </>
+        <img src={testImg} alt='test_img' height='300'/>
+        <Answer onClick={onClick} name='0'>{q.options[0]}</Answer>
+        <Answer onClick={onClick} name='1'>{q.options[1]}</Answer>
+        </>
       );
     }
-    else if(opNum===4){
+    else if(q.opNum===3){
       return (
         <>
-      <Answer onClick={onClick}>{questions[number].options[0]}</Answer>
-      <Answer onClick={onClick}>{questions[number].options[1]}</Answer>
-      <Answer onClick={onClick}>{questions[number].options[2]}</Answer>
-      <Answer onClick={onClick}>{questions[number].options[3]}</Answer>
-      </>
+        <Answer onClick={onClick} name='0'>{q.options[0]}</Answer>
+        <Answer onClick={onClick} name='1'>{q.options[1]}</Answer>
+        <Answer onClick={onClick} name='2'>{q.options[2]}</Answer>
+        </>
+      );
+    }
+    else if(q.opNum===4){
+      return (
+        <>
+        <Answer onClick={onClick} name='0'>{q.options[0]}</Answer>
+        <Answer onClick={onClick} name='1'>{q.options[1]}</Answer>
+        <Answer onClick={onClick} name='2'>{q.options[2]}</Answer>
+        <Answer onClick={onClick} name='3'>{q.options[3]}</Answer>
+        </>
       );
     }
   };
 
   return (
     <div>
-      <ProgressBar percent='50'/>
+      <ProgressBar percent={qNum}/>
       <div>
-        <Question number={number} question={questions[number].content}/>
+        <Question number={number} question={questions[index.current].content}/>
       </div>
       <div>
-      {makeQuestion(questions[number].opNum)}
-      </div>
-      <div class='pagebutton'>
-      <BackButton /> 
-      <NextButton />
+      {makeQuestion(questions[index.current])}
       </div>
     </div>
   );
