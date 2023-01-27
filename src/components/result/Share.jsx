2208425fa1b1c81ca56fdde246093592df954cbe
hanna-bae <122340442+hanna-bae@users.noreply.github.com>
2023-01-27 18:08:57 +0900

    modify share button

--- src/components/result/Share.jsx
+++ src/components/result/Share.jsx
@@ -6,33 +6,60 @@ import {
     FacebookIcon,
     TwitterShareButton,
     TwitterIcon,} from 'react-share';
-
+import { CopyToClipboard} from 'react-copy-to-clipboard';
+import ShareKaKao from './ShareKakao';
 const ButtonWrapper = styled.div`
-    margin : .2em;
+    display: grid;
+    grid-template-columns: repeat(4, 48px);
+    grid-column-gap:8px;
+    justify-content: center;
+    align-items: center;
+    margin-bottom : 16px;
 `;
 
 const Box = styled.div`
     display: flex;
+    flex-direction : column;
+    align-items : center;
 `;
 
+const URLshare = styled.div`
+    width : 48px;
+    height : 48px;
+    color : white;
+    border-radius: 24px;
+    border : 0px;
+    font-weight: 800;
+    font-size: 18px;
+    cursor : pointer;
+    background-color : #7362ff;
+    &:hover {
+        background-color : #a99fee;
+    }
+    text-align : center;
+    line-height : 48px;
+`;
 
 function Share() {
-    const shareUrl = 'https://localhost:3000/result';
+    const shareUrl = window.location.href;
 
     return (
         <div>
             <Box>
             <ButtonWrapper>
                 <FacebookShareButton url={shareUrl}>
-                    <FacebookIcon size={32} round />
+                    <FacebookIcon size={48} round />
                 </FacebookShareButton>
-            </ButtonWrapper>
-            <ButtonWrapper>
                 <TwitterShareButton url={shareUrl}>
-                    <TwitterIcon size={32} round />
+                    <TwitterIcon size={48} round />
                 </TwitterShareButton>
+                <CopyToClipboard text={shareUrl}>
+                    <URLshare>URL</URLshare>
+                </CopyToClipboard>
+                <ShareKaKao />
             </ButtonWrapper>
             </Box>
+            
         </div>
     );
 }
--- src/components/result/ShareKakao.jsx
+++ src/components/result/ShareKakao.jsx
@@ -1,65 +1,45 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
+import kakaotalk_icon from '../../assets/kakaotalk_icon.png';
+import styled from 'styled-components';
+import { useScript } from './hooks';
 
-const SharekaKao = () => {
-  useEffect(() => {
-    createKakaoButton()
-  }, [])
+const KakaoIcon= styled.img`
+  width : 48px;
+  height : 48px;
+  border-radius: 24px;
 
-  const createKakaoButton = () => {
-    // kakao sdk script이 정상적으로 불러와졌으면 window.Kakao로 접근이 가능합니다
-    if (window.Kakao) {
-      const kakao = window.Kakao
+`;
 
-      // 중복 initialization 방지
-      if (!kakao.isInitialized()) {
-        // 두번째 step 에서 가져온 javascript key 를 이용하여 initialize
-        kakao.init('d6a3544eb4ba298ff8a97e435df8251f')
-      }
+const KaKaoButton = styled.button`
+  cursor: pointer;
+  background : none;
+  border: none;
+`;
 
-      kakao.Link.createDefaultButton({
-        // Render 부분 id=kakao-link-btn 을 찾아 그부분에 렌더링을 합니다
-        container: '#kakao-link-btn',
-        objectType: 'feed',
-        content: {
-          title: '타이틀',
-          description: '#리액트 #카카오 #공유버튼',
-          imageUrl: 'IMAGE_URL', // i.e. process.env.FETCH_URL + '/logo.png'
-          link: {
-            mobileWebUrl: 'window.location.href',
-            webUrl: 'https://www.gist.ac.kr/kr/event_2st/index.html',
-          },
-        },
-        social: {
-          likeCount: 77,
-          commentCount: 55,
-          sharedCount: 333,
-        },
-        buttons: [
-          {
-            title: '웹으로 보기',
-            link: {
-              mobileWebUrl: window.location.href,
-              webUrl: window.location.href,
-            },
-          },
-          {
-            title: '앱으로 보기',
-            link: {
-              mobileWebUrl: window.location.href,
-              webUrl: window.location.href,
-            },
-          },
-        ],
-      })
-    }
-  }
+function ShareKaKao () {
+
+	const status = useScript("https://developers.kakao.com/sdk/js/kakao.js");
+	
+	
+	useEffect(() => {
+		if (status === "ready" && window.Kakao) {
+			if (!window.Kakao.isInitialized()) {
+				window.Kakao.init("d6a3544eb4ba298ff8a97e435df8251f");
+			}
+		}
+	}, [status]);	
+
+  const handleKakaoButton = () => {
+    window.Kakao.Link.sendScrap({
+        requestUrl: 'http://localhost:3000/result',
+    });
+};
 
   return (
-    <div className="kakao-share-button">
-      {/* Kakao share button */}
-      <button id="kakao-link-btn"/>
-    </div>
+    <KaKaoButton onClick={handleKakaoButton}>
+      <KakaoIcon src={kakaotalk_icon}></KakaoIcon>
+    </KaKaoButton>
   )
 }
 
-export default SharekaKao;
+export default ShareKaKao;
