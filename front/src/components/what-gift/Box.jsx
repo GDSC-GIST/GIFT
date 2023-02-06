aa1b00bc08d98bdfb47fc0059b6e371215ac4192
leessunj <sj06152@gmail.com>
2023-02-06 23:29:16 +0900

    flexible conveyer

---
+++ front/src/components/what-gift/Box.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from 'styled-components';
+
+const Svg = styled.svg`
+    display: flex; 
+    margin: 0px auto;
+    max-width: 72px;
+    @media screen and (max-width: 650px) {
+        max-width: 36px; 
+    }
+`;
+
+function Box({type}){
+    if(type===0){
+        return(
+        <Svg viewBox="0 0 72 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="72" height="60" fill="#F0D48C"/>
+            <rect x="7" width="15" height="30" fill="#C69256"/>
+        </Svg>
+        );
+    }
+    else{
+        return(
+            <Svg viewBox="0 0 72 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect width="72" height="60" fill="current"/>
+            </Svg>
+        );
+    }
+};
+
+export default Box;
